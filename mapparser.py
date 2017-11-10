d0c138a0bf9392a7d99433c04e3472ca96eb70ba
dnguyen22 <don.nguyen22@gmail.com>
2017-11-10 16:39:29 -0600

    Add example xml file to test on

--- mapparser.py
+++ mapparser.py
@@ -1,6 +1,8 @@
 import xml.etree.cElementTree as ET
 import pprint
 
+denver_boulder_OSM_file = "denver-boulder_colorado.osm"
+
 
 def count_tags(filename):
     tags = {}
@@ -11,8 +13,10 @@ def count_tags(filename):
 
 
 def test():
+    #tags = count_tags(denver_boulder_OSM_file)
     tags = count_tags('example.osm')
     pprint.pprint(tags)
+    """
     assert tags == {'bounds': 1,
                     'member': 3,
                     'nd': 4,
@@ -21,6 +25,7 @@ def test():
                     'relation': 1,
                     'tag': 7,
                     'way': 1}
+    """
 
 
 if __name__ == "__main__":
