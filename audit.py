1400cb560cd8adf81b367a49290e61f7e101ec99
dnguyen22 <don.nguyen22@gmail.com>
2017-11-10 19:22:14 -0600

    Fix street names

--- audit.py
+++ audit.py
@@ -8,7 +8,14 @@ osm_file = open("denver-boulder_colorado.osm", "r", encoding="utf8")
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 street_types = defaultdict(set)
 
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place"]
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
+            "Trail", "Parkway", "Commons"]
+
+mapping = {"St": "Street",
+           "St.": "Street",
+           "Rd.": "Road",
+           "Ave": "Avenue"
+           }
 
 
 def audit_street_type(street_types, street_name):
@@ -24,23 +31,50 @@ def print_sorted_dict(d):
     keys = sorted(keys, key=lambda s: s.lower())
     for k in keys:
         v = d[k]
-        print("%s: %d" % (k,v))
+        print("%s: %d" % (k, v))
 
 
 def is_street_name(elem):
     return elem.attrib['k'] == "addr:street"
 
 
-def audit():
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
     for event, elem in ET.iterparse(osm_file, events=("start",)):
         # if OSM street tag
-        if elem.tag == "way":
+        if elem.tag == "node" or elem.tag == "way":
             # loop through "tag" elements only
             for tag in elem.iter("tag"):
                 if is_street_name(tag):
                     audit_street_type(street_types, tag.attrib['v'])
-    pprint.pprint(dict(street_types))
+    osm_file.close()
+    return street_types
+
+
+def update_name(name, mapping):
+    name = name.split(" ")
+
+    if name[-1] in mapping:
+        name[-1] = mapping[name[-1]]
+
+    return " ".join(name)
+
+
+def test():
+    st_types = audit(osm_file)
+    assert len(st_types) == 3
+    pprint.pprint(dict(st_types))
+
+    for st_type, ways in st_types.iteritems():
+        for name in ways:
+            better_name = update_name(name, mapping)
+            print(name, "=>", better_name)
+            if name == "West Lexington St.":
+                assert better_name == "West Lexington Street"
+            if name == "Baldwin Rd.":
+                assert better_name == "Baldwin Road"
 
 
 if __name__ == '__main__':
-    audit()
+    test()
